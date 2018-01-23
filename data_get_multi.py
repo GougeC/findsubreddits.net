b2a5770974a9af8a31d01c0bb769aa0a533acb67
Connor Gouge <connorgouge@gmail.com>
2018-01-23 08:26:31 -0800

    added to db at post level not sub level

--- data_get_multi.py
+++ data_get_multi.py
@@ -49,7 +49,7 @@ def get_subreddits():
             subs.remove(x)
     return subs
 
-def get_post_info(post,user_list,client):
+def get_post_info(post,user_list,subreddit):
     '''
     Input: a PRAW post object
     Output: a post dictionary with data about the post imputed
@@ -59,6 +59,7 @@ def get_post_info(post,user_list,client):
     post_dict['title'] = post.title
     post_dict['id'] = post.id
     post_dict['permalink'] = post.permalink
+    post_dict['subreddit'] = subreddit
     if post.author.name:
         post_dict['author'] = post.author.name
         with open(user_list,'a') as f:
@@ -116,9 +117,9 @@ def get_10_children(comment,user_list):
         for reply in comment.replies:
             i+=1
             if i==10: break
-            if reply.author.name:
+            if reply.author:
                 with open(user_list,'a+') as h:
-                    h.write(reply.author.name)
+                    h.write(reply.author)
                     h.write(',\n')
                     h.flush()
             if reply.body:
@@ -131,8 +132,8 @@ def get_write_sub_data(sub_name,date,reddit,user_list,client):
     Retrives the data from the sub named in the sub_name parameter
     and writes the data as a json with the date included in the filename
     '''
-
-    print(sub_name)
+    t1 = time.time()
+    print("trying to get ",sub_name)
     try:
         sub = reddit.subreddit(sub_name)
     except:
@@ -147,23 +148,35 @@ def get_write_sub_data(sub_name,date,reddit,user_list,client):
     i = 0
     try:
         for post in top:
+            post_dic = {}
             i+=1
             try:
-                posts[post.id] = get_post_info(post,user_list,client)
+                post_dic['subreddit'] = sub_name
+                post_dic['permalink'] = post.permalink
+                post_dic['data'] = get_post_info(post,user_list,sub_name)
+                try:
+                    db = client.capstone_db
+                    posts = db.posts
+                    posts.insert(post_dic)
+                except Exception as e:
+                    print("failed to add post to db ",sub_name, e)
             except:
-                print('trying get_post_info broke',sub_name,i)
+                print('trying get_post_info broke',sub_name)
                 continue
 
     except:
         print('trying to loop through posts broke',sub_name)
         return None
 
-    filename = '../data'+date+'/'+sub_name+date+'.json'
-    print('writing ',sub_name," as ", filename)
-    subreddit_data = {'subreddit':sub_name,'posts':posts}
-    db = client.capstone_db
-    subs = db.subreddits
-    subs.insert(subreddit_data)
+    #filename = '../data'+date+'/'+sub_name+date+'.json'
+    #print('writing ',sub_name," as ", filename)
+    #subreddit_data = {'subreddit':sub_name,'posts':posts}
+    #db = client.capstone_db
+    #subs = db.subreddits
+    #subs.insert(subreddit_data)
+    t2 = time.time()
+    elapsed = t2 - t1
+    print("finished sub: {}. It took {} seconds.".format(sub_name,elapsed))
 
 
 client = pymongo.MongoClient('mongodb://ec2-34-211-151-30.us-west-2.compute.amazonaws.com:27017/')
@@ -193,32 +206,26 @@ for p in processes:
     counter+=1
 
 print('Processes Finished for subreddit data')
-for i in range(4):
-    open(directory+'users_list'+date+str(i)+'.txt','w').close()
-if counter==4:
-    users1 = pd.read_csv(directory+'users_list'+date+str(1)+'.txt',header=None)[0]
-    users2 = pd.read_csv(directory+'users_list'+date+str(2)+'.txt',header=None)[0]
-    users3 = pd.read_csv(directory+'users_list'+date+str(3)+'.txt',header=None)[0]
-    users4 = pd.read_csv(directory+'users_list'+date+str(4)+'.txt',header=None)[0]
+users1 = pd.read_csv(directory+'users_list'+date+str(1)+'.txt',header=None)[0]
+users2 = pd.read_csv(directory+'users_list'+date+str(2)+'.txt',header=None)[0]
+users3 = pd.read_csv(directory+'users_list'+date+str(3)+'.txt',header=None)[0]
+users4 = pd.read_csv(directory+'users_list'+date+str(4)+'.txt',header=None)[0]
+bagousers = set()
 
-    bagousers = set()
-    user
-    for lst in [users1,users2,users3,users4]:
-        uniq = lst.unique()
-        for user in uniq:
-            bagousers.add(user)
-    users_unique = list(bagousers)
-    k = len(users_unique_list)//4
-    lists = [users_unique[:k],users_unique[k:2*k],users_unique[2*k,3*k],users_unique[3*k:]]
-
-    for i in range(1,5):
-        keys = np.loadtxt('keys/reddit{}.txt'.format(i),dtype=str,delimiter=',')
-        filename = '../data'+date+'/'+'USER_DATA_'+str(i)+'.json'
-        p = Process(target=grud.get_data_for_userlist, args = (lists[i-1],keys,filename,client))
-        processes.append(p)
-
-    for p in processes:
-        p.start()
-    for p in processes:
-        p.join()
-    print('Processes Finished for subreddit data')
+for lst in [users1,users2,users3,users4]:
+    uniq = lst.unique()
+    for user in uniq:
+        bagousers.add(user)
+users_unique = list(bagousers)
+k = len(users_unique_list)//4
+lists = [users_unique[:k],users_unique[k:2*k],users_unique[2*k,3*k],users_unique[3*k:]]
+for i in range(1,5):
+    keys = np.loadtxt('keys/reddit{}.txt'.format(i),dtype=str,delimiter=',')
+    filename = '../data'+date+'/'+'USER_DATA_'+str(i)+'.json'
+    p = Process(target=grud.get_data_for_userlist, args = (lists[i-1],keys,filename,client))
+    processes.append(p)
+for p in processes:
+    p.start()
+for p in processes:
+    p.join()
+print('Processes Finished for User Data')
